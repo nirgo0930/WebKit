419e4f1f38b81db9efec5a52ffcdeaeadea7c2db
nirgo0930 <94219157+nirgo0930@users.noreply.github.com>
2022-07-26 11:15:02 +0900

    05

---
+++ CSS_WorkSpace/server.js
@@ -0,0 +1,25 @@
+const http = require('http');
+const express = require('express');
+const app = express();
+const cors = require('cors');
+
+const Port = 3000;
+
+app.get('/', (req, res, next) => {
+    res.end("<h1>Welcome</h1>");
+});
+
+app.get('/test', (req, res, next) => {
+    res.end("<h1>Welcome Test</h1>");
+});
+
+//pubcic 폴더 static 설정
+app.use(express.static('public'));
+
+
+
+
+const server = http.createServer(app);
+server.listen(Port, function () {
+    console.log(`Server running at http://localhost:${Port}`);
+});
