60f72822712ecb30be858650c9b6df7a2cc69b16
nirgo0930 <94219157+nirgo0930@users.noreply.github.com>
2022-08-03 16:29:41 +0900

    0803_1630

---
+++ react_workspace/my-react-app/src/TodoList.js
@@ -0,0 +1,37 @@
+import './App.css';
+import React from 'react';
+
+class TodoList extends React.Component {
+    constructor(props) {
+        console.log(props.title)
+        super(props);
+
+        this.state = {
+            data: "hahahaha"
+        };
+    }
+
+    render() {
+        let styleObj = { color: 'red', textDecoration: 'underline', backgroundColor: 'yellow' };
+
+        return (<>
+            <h2>Todo List</h2>
+            <div>
+                <label htmlFor="">할 일 : </label>
+                <input type="text" value={this.state.data} onChange={e => {
+                    this.setState({ data: e.currentTarget.value });
+                }} />
+                <div id="status">상태 : {this.state.data}</div>
+                <ol>
+                    <li>
+                        <span style={styleObj} className="strike">프로젝트 기획하기</span>
+                        <button>완료</button>
+                        <button>삭제</button>
+                    </li>
+                </ol>
+            </div>
+        </>);
+    }
+}
+
+export default TodoList;
